96ec06505da0e64a8d4736ecd44f9e1a5176b872
james-ogunsanwo <james.ogunsanwo@mettle.co.uk>
2023-01-29 21:05:50 +0000

    testing script

--- .github/scripts/performance-test-matrix.sh
+++ .github/scripts/performance-test-matrix.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "$CURRENT_MATRIX"
+
 filteredMatrix=$(echo "$CURRENT_MATRIX" | jq '.include[] | select(.featureTest == "true")' | jq -n '.include |= [inputs]' | jq -s -c .[])
 echo "performance_test_matrix=$filteredMatrix" >> $GITHUB_OUTPUT
 echo "performance_test_matrix=$filteredMatrix"
