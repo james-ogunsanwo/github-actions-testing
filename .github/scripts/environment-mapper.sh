8ccec2fc864bcfbd6cd5056886a939cefeb8a796
james-ogunsanwo <james.ogunsanwo@mettle.co.uk>
2023-01-30 20:58:36 +0000

    testing script

--- .github/scripts/environment-mapper.sh
+++ .github/scripts/environment-mapper.sh
@@ -11,5 +11,5 @@ eval .github/scripts/convert-configuration.sh dev
 eval .github/scripts/convert-configuration.sh test
 
 jq -n '{dev: $dev, test: $test}' \
-  --arg dev "$(jq . dev-config.json)" \
-  --arg test "$(jq . test-config.json)" | outputMatrix
+  --arg dev "$(cat dev-config.txt)" \
+  --arg test "$(cat test-config.txt)" | outputMatrix
