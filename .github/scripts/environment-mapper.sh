b7eee2ab6d86218ea525beff3a2777c93780762a
james-ogunsanwo <james.ogunsanwo@mettle.co.uk>
2023-01-30 22:07:54 +0000

    testing script

--- .github/scripts/environment-mapper.sh
+++ .github/scripts/environment-mapper.sh
@@ -2,8 +2,8 @@
 
 outputMatrix() {
   data=$(jq -n '. |= inputs' | jq -c .)
-  echo "matrixx=$(jq -n '. |= inputs' | jq -c .)" >> "$GITHUB_OUTPUT"
-  echo "matrixx=$(jq -n '. |= inputs' | jq -c .)"
+  echo "matrix=$(jq -n '. |= inputs' | jq -c .)" >> "$GITHUB_OUTPUT"
+  echo "matrix=$(jq -n '. |= inputs' | jq -c .)"
 }
 
 eval .github/scripts/convert-configuration.sh dev
