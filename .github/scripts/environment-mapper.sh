38ebea0045673ec7c5d201688709f3b7b3573e5e
james-ogunsanwo <james.ogunsanwo@mettle.co.uk>
2023-01-31 09:18:30 +0000

    testing script

--- .github/scripts/environment-mapper.sh
+++ .github/scripts/environment-mapper.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
 
+#outputMatrix() {
+#  data=$(jq -n '. |= $1' | jq -c .)
+#  echo "$2_matrix=$data" >>"$GITHUB_OUTPUT"
+#  echo "$data"
+#}
+#
+#eval .github/scripts/convert-configuration.sh dev
+#eval .github/scripts/convert-configuration.sh test
+#
+##jq -n '{dev: $dev, test: $test}' \
+##  --argjson dev "$(cat dev-config.txt)" \
+##  --argjson test "$(cat test-config.txt)" | outputMatrix
+#
+#outputMatrix $(cat dev-config.txt) dev
+#outputMatrix $(cat test-config.txt) test
+
+
 outputMatrix() {
   data=$(jq -n '. |= inputs' | jq -c .)
-  echo "matrix=$data" >> "$GITHUB_OUTPUT"
-  echo "matrix=$data"
+  echo "$1_matrix=$data" >>"$GITHUB_OUTPUT"
+  echo "$data"
 }
 
 eval .github/scripts/convert-configuration.sh dev
 eval .github/scripts/convert-configuration.sh test
 
-jq -n '{dev: $dev, test: $test}' \
-  --argjson dev "$(cat dev-config.txt)" \
-  --argjson test "$(cat test-config.txt)" | outputMatrix
+jq -n "$(cat dev-config.json)"  | outputMatrix dev
+jq -n "$(cat test-config.json)"  | outputMatrix test
